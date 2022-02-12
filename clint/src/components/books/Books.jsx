a546d2fa08bb6d474024df0e98f0ad6bfb823c24
Turki Haqawi <83429676+TurkiHaqawi@users.noreply.github.com>
2022-02-13 00:56:20 +0300

    Update Books.jsx

--- clint/src/components/books/Books.jsx
+++ clint/src/components/books/Books.jsx
@@ -4,7 +4,9 @@ import Book from "../book/Book";
 function Books({ books }) {
   return (
     <div className="books">
-      
+      {books.map((b, index) => (
+        <Book book={b} key={index} />
+      ))}
     </div>
   );
 }
